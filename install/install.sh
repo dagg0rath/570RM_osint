6123a55c1b7783b071497b8a1b52b23944d3d517
mrcripoll <58988471+mrcripoll@users.noreply.github.com>
2022-03-29 18:14:22 +0100

    Create install.sh

---
+++ install/install.sh
@@ -0,0 +1,13 @@
+echo "\n[+] Installing Dependencies...\n"
+sudo apt update
+sudo apt install curl
+sudo apt install whois
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install mtr
+pip3 install --user --upgrade git+https://github.com/himanshudabas/twint.git@origin/twint-fixes#egg=twint
+
+echo "\n[+] Installing Python Modules...\n"
+pip3 install -r pip_requirements.txt
+
+echo "\n[=] Installation completed! \n"
